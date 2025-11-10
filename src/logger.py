16ac265d2595c50153926109678a26a2c31fe70b
Man Kumar Tamang <nissanlama2020@gmail.com>
2025-11-10 14:41:31 +0545

    Logging & Exception

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Generate log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Define logs directory
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
+# Define full log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
 )
+
+# Expose the logging object
+logger=logging
